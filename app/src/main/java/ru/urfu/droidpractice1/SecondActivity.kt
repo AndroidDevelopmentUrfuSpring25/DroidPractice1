1825666780478fb65743d9e57294b3f8f4db2aec
Sofia <sofia.sofia>
2025-02-22 20:44:09 +0500

    first commit

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -3,10 +3,13 @@ package ru.urfu.droidpractice1
 import androidx.activity.ComponentActivity
 import android.os.Bundle
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
+import com.bumptech.glide.Glide
+import ru.urfu.droidpractice1.model.ARTICLE2_IMAGE_URL
 
 class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    private var isReadChecked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +18,16 @@ class SecondActivity : ComponentActivity() {
         setContentView(view)
 
         binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+        Glide.with(binding.photo)
+            .asBitmap()
+            .load(ARTICLE2_IMAGE_URL)
+            .into(binding.photo)
+
+        binding.readSwitch.setOnCheckedChangeListener { _, isChecked ->
+            isReadChecked = isChecked
+        }
+
+
     }
 }
---
+++ app/src/main/java/ru/urfu/droidpractice1/model/ArticleScreenData.kt
@@ -0,0 +1,3 @@
+package ru.urfu.droidpractice1.model
+
+const val ARTICLE2_IMAGE_URL: String = "https://img.championat.com/s/732x488/news/big/u/i/igrok-krylev-kostanca-obyasn.jpg"
