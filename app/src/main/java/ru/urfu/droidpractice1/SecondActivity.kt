016af63b8f1ebc90825792ec795665ab6b2280ab
kurto <kurtovnevg@gmail.com>
2025-02-24 12:05:05 +0500

    practice#1 first part

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -3,10 +3,12 @@ package ru.urfu.droidpractice1
 import androidx.activity.ComponentActivity
 import android.os.Bundle
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
+import com.bumptech.glide.Glide
 
 class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    private var isReadChecked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +16,14 @@ class SecondActivity : ComponentActivity() {
         val view = binding.root
         setContentView(view)
 
+        Glide.with(binding.image1)
+            .asBitmap()
+            .load("https://img.championat.com/s/732x488/news/big/i/p/aleksandr-ovechkin-nazval-buduschego-kapitana-vashingtona_17403646002007547425.jpg")
+            .into(binding.image1)
+
         binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+        binding.switchRead.setOnCheckedChangeListener { _, isChecked ->
+            isReadChecked = isChecked}
     }
 }
