2fbdac462473add47f3dc2529bebfdeb04b5b76a
Roman <125725530+RomanGleizer@users.noreply.github.com>
2025-02-24 19:19:34 +0500

    Droid1

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,19 +1,65 @@
 package ru.urfu.droidpractice1
 
-import androidx.activity.ComponentActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import com.bumptech.glide.Glide
+import androidx.activity.ComponentActivity
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
 
 class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    private lateinit var prefs: SharedPreferences
+
+    companion object {
+        private const val TAG = "SecondActivity"
+        private const val READ_STATUS_KEY = "read_status"
+        private const val IMAGE_URL = "https://img.championat.com/s/732x488/news/big/e/o/skazhu-odno-my-budem-gotovy-simeone-o-madridskom-derbi-v-1-8-finala-ligi-chempionov_1740169817524784904.jpg"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         binding = ActivitySecondBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
+        prefs = getSharedPreferences("article_prefs", MODE_PRIVATE)
         binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+        Glide.with(this)
+            .load(IMAGE_URL)
+            .into(binding.articleImage)
+
+        binding.readSwitch.isChecked = prefs.getBoolean(READ_STATUS_KEY, false)
+        binding.readSwitch.setOnCheckedChangeListener { _, isChecked ->
+            prefs.edit().putBoolean(READ_STATUS_KEY, isChecked).apply()
+            Log.d(TAG, "Переключатель 'Прочитано' изменён: $isChecked")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }
--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -5,10 +5,7 @@ package ru.urfu.droidpractice1.content
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -18,9 +15,17 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import ru.urfu.droidpractice1.R
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
+import android.content.Intent
+import androidx.compose.material3.Button
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import ru.urfu.droidpractice1.SecondActivity
 
 @Composable
 fun MainActivityScreen() {
+
+    val context = LocalContext.current
+
     DroidPractice1Theme {
         Scaffold(modifier = Modifier.fillMaxSize(),
             topBar = {
@@ -37,11 +42,19 @@ fun MainActivityScreen() {
             ) {
 
             }
+            Box(modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)) {
+
+                Button(onClick = {
+                    context.startActivity(Intent(context, SecondActivity::class.java))
+                }) {
+                    Text(text = "Перейти ко второй статье")
+                }
+            }
         }
     }
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
