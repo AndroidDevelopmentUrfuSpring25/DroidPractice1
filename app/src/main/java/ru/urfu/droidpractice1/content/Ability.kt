fbb6a3044c7db00a8226dd66ad998f33fe5d7292
Almazova Vlada <almazova.2004@mail.ru>
2025-03-03 22:29:00 +0500

    стили и картинка 1 экран

---
+++ app/src/main/java/ru/urfu/droidpractice1/content/Ability.kt
@@ -0,0 +1,7 @@
+package ru.urfu.droidpractice1.content
+
+data class Ability(
+    val name: String,
+    val type: String,
+    val description: String
+)
--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -2,22 +2,34 @@
 
 package ru.urfu.droidpractice1.content
 
+//import android.graphics.fonts.FontStyle
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.urfu.droidpractice1.R
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
+import coil.compose.AsyncImage
+import androidx.compose.ui.text.font.FontStyle
 
 @Composable
 fun MainActivityScreen() {
@@ -35,7 +47,32 @@ fun MainActivityScreen() {
             Box(
                 modifier = Modifier.padding(innerPadding)
             ) {
-
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState())
+                        .padding(30.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.heading1),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = stringResource(id = R.string.general_description),
+                        fontSize = 16.sp,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    DownloadImg(DataMainActivity.imageUrl)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    DataMainActivity.abilities.forEach { ability ->
+                        AbilityItem(ability)
+                        Spacer(modifier = Modifier.height(12.dp))
+                    }
+                }
             }
         }
     }
@@ -47,4 +84,39 @@ fun MainActivityScreen() {
 @Composable
 fun MainScreenPreview() {
     MainActivityScreen()
+}
+
+@Composable
+fun AbilityItem(ability: Ability) {
+    Column(modifier = Modifier
+        .fillMaxSize()) {
+        Text(
+            text = ability.name,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = ability.type,
+            fontSize = 14.sp,
+            fontStyle = FontStyle.Italic
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = ability.description,
+            fontSize = 14.sp
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+}
+
+@Composable
+fun DownloadImg(imageUrl: String) {
+    AsyncImage(
+        model = imageUrl,
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+    )
 }
