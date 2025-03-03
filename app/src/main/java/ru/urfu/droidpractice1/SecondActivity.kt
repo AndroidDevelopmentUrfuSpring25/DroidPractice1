fc7f2769306ed72eaff56ce850a2dcc76f58171d
Almazova Vlada <almazova.2004@mail.ru>
2025-03-03 17:43:48 +0500

    стили и картинка 2 экран

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -2,6 +2,8 @@ package ru.urfu.droidpractice1
 
 import androidx.activity.ComponentActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
 
 class SecondActivity : ComponentActivity() {
@@ -13,6 +15,12 @@ class SecondActivity : ComponentActivity() {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        // Загрузка картинки
+        val imageView: ImageView = findViewById(R.id.Fatutu_2_img)
+        val imageUrl = "https://cdn.donmai.us/sample/d2/bb/__fatutu_reverse_1999__sample-d2bbbdc0dfcd7dba3b6a0a854e9ef943.jpg"
+        Glide.with(this)
+            .load(imageUrl)
+            .into(imageView)
 
         binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
