f97f067862098d736cbd745548046868d6eeb00e
Roman Mesler <meslerdev@gmail.com>
2025-03-16 14:07:22 +0500

    fix review issues

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -13,6 +13,8 @@ import ru.urfu.droidpractice1.SecondActivity.Companion.KEY_READ
 import ru.urfu.droidpractice1.content.LESSON_LOG
 import ru.urfu.droidpractice1.content.MainActivityScreen
 
+const val LIFECYCLE_TAG = "LIFECYCLE_LOG"
+
 class MainActivity : ComponentActivity() {
     private var read: Boolean by mutableStateOf(false)
 
@@ -26,9 +28,40 @@ class MainActivity : ComponentActivity() {
     }
 
     private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        Log.d(LESSON_LOG, "inside register for activity result")
         if (result.resultCode == RESULT_OK) {
             val data: Intent? = result.data
             read = data?.getBooleanExtra(KEY_READ, false) ?: false
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(LIFECYCLE_TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LIFECYCLE_TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LIFECYCLE_TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LIFECYCLE_TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LIFECYCLE_TAG, "onDestroy called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(LIFECYCLE_TAG, "onRestart called")
     }
 }
